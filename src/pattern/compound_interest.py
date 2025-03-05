1120f100cff9143f2c528ce9e386120c91a0d529
YoshikiGit <ysklove.music1stsight@gmail.com>
2025-03-06 00:02:43 +0900

    Add logic of lifetime_growth_frame

--- src/pattern/compound_interest.py
+++ src/pattern/compound_interest.py
@@ -1,22 +1,32 @@
 # 利率
-interest_rate = 1.102
+INTEREST_RATE = 1.102
 # 年間投資額
-annually_investment_amount = 400000
+ANNUALLY_INVESTMENT_AMOUNT = 1200000
 # 投資年数
-num_investment = 20
+NUM_INVESTMENT = 20
+# 生涯成長投資枠（1,200万円）
+lifetime_growth_frame = 12000000
 
-def compound_interest(annually_investment_amount, num_investment, annually_change_list) -> float:
-    if num_investment == 0:
+def compound_interest(ANNUALLY_INVESTMENT_AMOUNT, NUM_INVESTMENT, annually_change_list, lifetime_growth_frame) -> float:
+    if NUM_INVESTMENT == 0:
         return sum(annually_change_list)
     
-    # 新たな投資額を追加
-    annually_change_list.append(annually_investment_amount)
+    if lifetime_growth_frame < 0:
+        annually_change_list.append(0)
+    else:
+        lifetime_growth_frame = lifetime_growth_frame - ANNUALLY_INVESTMENT_AMOUNT
+        if lifetime_growth_frame <= 0:
+            # 限度額を超えた場合、残枠の金額を追加
+            annually_change_list.append(ANNUALLY_INVESTMENT_AMOUNT + lifetime_growth_frame)
+        else:
+            # 新たな投資額を追加
+            annually_change_list.append(ANNUALLY_INVESTMENT_AMOUNT)
 
     # 既存の資金を複利で成長させる（リストの内容を更新）
     for i in range(len(annually_change_list)):
-        annually_change_list[i] = annually_change_list[i] * interest_rate
+        annually_change_list[i] = annually_change_list[i] * INTEREST_RATE
         
     
-    return compound_interest(annually_investment_amount, num_investment - 1, annually_change_list)
+    return compound_interest(ANNUALLY_INVESTMENT_AMOUNT, NUM_INVESTMENT - 1, annually_change_list, lifetime_growth_frame)
 
-print(compound_interest(annually_investment_amount, num_investment, []))
+print(compound_interest(ANNUALLY_INVESTMENT_AMOUNT, NUM_INVESTMENT, [], lifetime_growth_frame))
